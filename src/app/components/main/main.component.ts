8796a2225948bae45f4008a94c2ae4a61d91740b
Stephen Kirkland <skirkland.job@gmail.com>
2022-07-30 02:11:04 -0400

    Making the information so that I can change the names by variables.

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -10,18 +10,27 @@ import { VCard } from "ngx-vcard";
 })
 export class MainComponent implements OnInit {
 
+  public firstname = `Stephen`;
+  public lastname = `Kirkland`;
+  public fullname;
+  public nickname = `BigMoneyKirk`;
+  public occupation = `Website Developer`;
+  public phonenumber = `912-536-7419`;
+  public businessemail = `skirkland.job@gmail.com`;
+
   constructor(public imageService: ImageService) { }
 
   ngOnInit() {
+    this.fullname = `${this.firstname} "${this.nickname}" ${this.lastname}`;
   }
 
   public vCard: VCard = {
     name: {
-      firstNames: "Stephen",
-      lastNames: "Kirkland",
+      firstNames: this.firstname,
+      lastNames: this.lastname,
     },
-    telephone: ["912-536-7419"],
-    email: ["skirkland.job@gmail.com"],
+    telephone: [this.phonenumber],
+    email: [this.businessemail],
     workEmail: ["kirklandenterprisesunlimited@gmail.com"],
     role: "CEO of Kirkland Enterprises Unlimited",
     url: "https://BigMoneyKirk.github.io/QR/",
