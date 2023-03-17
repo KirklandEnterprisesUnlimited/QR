debc49b1ffc403bf688e5eaa4a8dc9625d17f718
BigMoneyKirk <skirkland.job@gmail.com>
2023-03-17 01:20:01 -0400

    Configured Firenase credentials with the start of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QR';
+  firebaseConfig = {
+    apiKey: "AIzaSyCB047MRag3zpuhK6IPlOBTBEJnX7FTTc0",
+    authDomain: "vcard-keu.firebaseapp.com",
+    projectId: "vcard-keu",
+    storageBucket: "vcard-keu.appspot.com",
+    messagingSenderId: "1068501198675",
+    appId: "1:1068501198675:web:af65a1d3eb0055f77f63a6",
+    measurementId: "G-Z8KVQWSRQ5"
+  };
+
+  ngOnInit() {
+    const app = initializeApp(this.firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { MainComponent } from './components/main/main.component';
 import { ImageService } from './services/image.service';
 
 import { NgxVcardModule } from "ngx-vcard";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
