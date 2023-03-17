441b5cff08a86473dddb4561de5285cb2f3bb878
BigMoneyKirk <skirkland.job@gmail.com>
2023-03-17 00:42:22 -0400

    I'm okay with all of these changes. Timem to merge to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { MainComponent } from './components/main/main.component';
 import { ImageService } from './services/image.service';
 
 import { NgxVcardModule } from "ngx-vcard";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
